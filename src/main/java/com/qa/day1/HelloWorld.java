451a50fa57e1d926d068220b09d15a641d54702a
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-10 22:56:47 +0100

    created method printMessage and called in main method

--- src/main/java/com/qa/day1/HelloWorld.java
+++ src/main/java/com/qa/day1/HelloWorld.java
@@ -2,9 +2,13 @@ package com.qa.day1;
 
 public class HelloWorld {
 
-    public static void main(String[] arg) {
+    public static void printMessage() {
         String message = "Hello World!";
         System.out.println(message);
+    }
+
+    public static void main(String[] arg) {
+        printMessage();
     }
 
 }
