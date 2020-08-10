9adbe161c1d48e9963f2247d3f6167f7944ab5f5
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-10 23:00:55 +0100

    refactored code so that message is passed as parameter and printMessage is called in main method.

--- src/main/java/com/qa/day1/HelloWorld.java
+++ src/main/java/com/qa/day1/HelloWorld.java
@@ -2,13 +2,12 @@ package com.qa.day1;
 
 public class HelloWorld {
 
-    public static void printMessage() {
-        String message = "Hello World!";
+    public static void printMessage(String message) {
         System.out.println(message);
     }
 
     public static void main(String[] arg) {
-        printMessage();
+        printMessage("Hello World!");
     }
 
 }
