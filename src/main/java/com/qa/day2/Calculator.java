124dff0efa88fe1dd375b22a98a5ae5d10cb9417
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-11 22:22:04 +0100

    got rid of unecessary comments in Calculator

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -1,14 +1,5 @@
 package com.qa.day2;
 
-// Create a method that accepts two integers as input, then returns an integer that is the sum of these two parameters. (Addition)
-// Create the following additional three methods that each take two parameters:
-// Multiplication - which takes two numbers and returns the product.
-// Subtraction - which takes two numbers, then returns the result of the subtraction.
-// Division - which takes two numbers, then returns the result of the division.
-// Your division method may have returned the wrong result; This is called a rounding error and is quite common in most languages. this is because we were using ints rather than doubles. Modify the division method so that it takes Double parameters and then return a Double, if not already.
-// With the division method, the sum should only execute if the first parameter is smaller than the second, otherwise it prints out a message saying that the division cannot be performed.
-
-
 import java.util.Scanner;
 
 public class Calculator {
