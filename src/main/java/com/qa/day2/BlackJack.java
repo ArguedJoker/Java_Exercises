3d2a579b90e80dcfed388684e14566bb7a06df98
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-12 21:36:04 +0100

    completed 3rd challenge in conditionals module where depending on value of input A or input B the highest number that is closest to 21 (without going over 21) will be printed out. If both A and B are over 21 0 will be printed out.

--- src/main/java/com/qa/day2/BlackJack.java
+++ src/main/java/com/qa/day2/BlackJack.java
@@ -1,9 +1,33 @@
 package com.qa.day2;
 
-// Given 2 integer values greater than 0, return whichever is closest to 21 without going over 21. If they both go over 21 then return 0.
+import java.util.Scanner;
 
 public class BlackJack {
     public static void main (String[] arg) {
 
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter your first number which must be higher than 0: ");
+        String number1 = input1.nextLine();
+        int a = Integer.parseInt(number1);
+        if (a == 0) {
+            System.out.println("You chose zero! Y u do dis? Try again!");
+            }
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter your second number which must also be over 0: ");
+        String number2 = input2.nextLine();
+        int b = Integer.parseInt(number2);
+        if (b == 0) {
+            System.out.println("You chose zero! Y u do dis? Goodbye");
+        }
+        if (a <= 21 && b <= 21){
+            System.out.println(Math.max(a, b));
+        }
+        if (a > 21){
+          System.out.println("0");
+        }
+        if (b > 21){
+            System.out.println("0");
+        }
     }
 }
