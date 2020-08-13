859597fd61839e3a8c96f2b0249c0a8497c55876
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-13 09:48:38 +0100

    updated 1st FLowChart exercise in conditional module. Inut being taken as intended now.

--- src/main/java/com/qa/day2/FlowCharts.java
+++ src/main/java/com/qa/day2/FlowCharts.java
@@ -8,15 +8,12 @@ public class FlowCharts {
     public static void main(String[] arg) {
         Scanner input1 = new Scanner(System.in);
         System.out.println("Enter a number: ");
-
         String number1 = input1.nextLine();
         int a = Integer.parseInt(number1);
         System.out.println("You chose: " + a);
 
         Scanner input2 = new Scanner(System.in);
-
         System.out.println("Enter a number: ");
-
         String number2 = input2.nextLine();
         int b = Integer.parseInt(number2);
         System.out.println("You Chose: " + b);
