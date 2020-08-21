def554c468ac4848945bd8b18404215c13357d7f
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-21 17:16:43 +0100

    practicing Java and TDD for assessment

---
+++ src/main/java/com/qa/day9/JavaPractice.java
@@ -0,0 +1,17 @@
+package com.qa.day9;
+
+public class JavaPractice {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.5;
+        char studentFirstInitial = 'R';
+        char studentLastInitial = 'C';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
