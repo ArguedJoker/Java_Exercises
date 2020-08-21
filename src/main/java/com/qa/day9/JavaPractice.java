1d29c11dfecf070c9fac65b8d04fc86f1008dae6
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-21 18:33:35 +0100

    practice using primitive data types, data types and variables and using strings. A little bit of practicing using indexes with strings and concatenation.

--- src/main/java/com/qa/day9/JavaPractice.java
+++ src/main/java/com/qa/day9/JavaPractice.java
@@ -4,14 +4,20 @@ public class JavaPractice {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.5;
-        char studentFirstInitial = 'R';
-        char studentLastInitial = 'C';
         boolean hasPerfectAttendance = true;
+
+        String studentForeName = "Rohan";
+        String studentLastName = "Chauhan";
+        char studentFirstInitial = studentForeName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentForeName);
+        System.out.println(studentLastName);
+        System.out.println(studentForeName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
---
+++ src/test/java/com/qa/day1/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package com.qa.day1;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+
+}
