cdab14be393828f9a815e334abaaf3aa6ce80b1c
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-22 18:14:13 +0100

    - more practice defining a function and writing code outside of main  - practiced using return in function and using the returned value to work out how much each person owes for the meal

---
+++ src/main/java/com/qa/day10/Finance.java
@@ -0,0 +1,20 @@
+package com.qa.day10;
+
+public class Finance {
+
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .20, .08);
+        System.out.println(groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
+    }
+}
