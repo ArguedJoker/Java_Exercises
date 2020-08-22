1365272351f6b4974c291888131c50e2a5c8c921
ArguedJoker <rohanpschauhan@gmail.com>
2020-08-22 19:02:25 +0100

    - corrected calculation

--- src/main/java/com/qa/day10/EmployeeSalary.java
+++ src/main/java/com/qa/day10/EmployeeSalary.java
@@ -4,22 +4,30 @@ public class EmployeeSalary {
     // input number of hours the employee works per week
     // input amount of number employee makes per hour
     // output employee's gross yearly salary (before tax)
-    // stretch goal - add input for vacation days (employee does not get paid for vacation days taken
+    // stretch goal - add input for vacation days (employee does not get paid for vacation days taken)
 
     public static double employeeWeeklySalary(double totalHoursPerWeek, double payPerHour) {
+        if (totalHoursPerWeek < 0) {
+            return -1;
+        }
+        if (payPerHour < 0) {
+            return -1;
+        }
         double totalPayPerWeek = totalHoursPerWeek * payPerHour;
+
 
         return totalPayPerWeek;
     }
 
-    public static double employeeVacation(int numberOfDaysVacation, double normalEmployeeDayHours) {
-        double totalTimeVacationUnpaid = numberOfDaysVacation * normalEmployeeDayHours;
+    public static double employeeVacation(int numberOfDaysVacation, double normalEmployeeDayHours, double payPerHour) {
+
+        double totalTimeVacationUnpaid = numberOfDaysVacation * normalEmployeeDayHours * payPerHour;
 
         return totalTimeVacationUnpaid;
     }
 
     public static void main(String[] args) {
-        double employeeSalary = employeeWeeklySalary(40, 21) * 4 * 12 - employeeVacation(5, 8);
+        double employeeSalary = employeeWeeklySalary(40, 21) * 4 * 12 - employeeVacation(5, 8, 21);
         System.out.println(employeeSalary);
     }
 }
